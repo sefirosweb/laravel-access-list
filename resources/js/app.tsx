8efa45fea0c41ebee2ef909d5d8ab773aeae09ab
Sefirosweb <sefirosweb@gmail.com>
2022-12-20 15:00:03 +0000

    Fix bug with react query

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import RoutesConfig from "@/routes/RoutesConfig";
+import { useGetQueryClient } from "@sefirosweb/react-crud";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 if (document.getElementById("root")) {
   const root = createRoot(document.getElementById("root"));
 
+  const queryClient = useGetQueryClient()
+
   root.render(
     <React.StrictMode>
-      <RoutesConfig />
+      <QueryClientProvider client={queryClient}>
+        <RoutesConfig />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }

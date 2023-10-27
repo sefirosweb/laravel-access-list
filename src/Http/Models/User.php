9b5f8d95d9953aa39158f0ac56fe30bcdaff2c02
Sefirosweb <sefirosweb@gmail.com>
2023-10-27 21:11:07 +0000

    get acl list and has role validation

--- src/Http/Models/User.php
+++ src/Http/Models/User.php
@@ -24,4 +24,26 @@ class User extends Authenticatable
         $Role = config('laravel-access-list.Role');
         return $this->belongsToMany($Role, 'user_has_role');
     }
+
+    public function getAclList()
+    {
+        return array_values(array_unique($this->roles->reduce(function ($carry, $role) {
+            return array_merge($carry, array_column($role->access_lists->toArray(), 'name'));
+        }, [])));
+    }
+
+    public function hasAcl($acl)
+    {
+        $acl_list = $this->getAclList();
+
+        if (in_array('admin', $acl_list)) {
+            return true;
+        }
+
+        if (in_array($acl, $acl_list)) {
+            return true;
+        }
+
+        return false;
+    }
 }
